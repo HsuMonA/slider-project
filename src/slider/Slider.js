71d3436527c32fab8a6605691a4531c6adb16179
HsuMonA <HsuMonA@users.noreply.github.com>
2023-02-25 17:28:47 +0100

    create slider style

---
+++ src/slider/Slider.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Slider.css";
+
+
+function Slider() {
+    
+  return (
+    <div className="style-container">
+      <div className="ellipse"></div>
+      <div className="text">3.5</div>
+      <div className="range-base"></div>
+    </div>
+  );
+}
+
+export default Slider;
