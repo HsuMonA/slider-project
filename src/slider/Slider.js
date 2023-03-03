d90451968b87bc2fe895f43b092497c28a784d35
HsuMonA <HsuMonA@users.noreply.github.com>
2023-03-03 18:21:36 +0100

    make thumb div move horizontally

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -1,16 +1,54 @@
-import React from "react";
-
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import "./Slider.css";
 
+function Slider(props) {
+  const { min, max } = props;
+  const isDragging = useRef(false);
+  const thumbRef = useRef();
+  const [position, setPosition] = useState(0);
 
-function Slider() {
-    
+  const onMouseDown = useCallback((event) => {
+    if (thumbRef.current && thumbRef.current.contains(event.target)) {
+      isDragging.current = true;
+    }
+  }, []);
+
+  const onMouseUp = useCallback(() => {
+    if (isDragging.current) {
+      isDragging.current = false;
+    }
+  }, []);
+
+  const onMouseMove = useCallback((event) => {
+    if (isDragging.current) {
+      setPosition((position) => {
+        if (position <= 0 && event.movementX < 0) {
+          return 0;
+        } else if (position >= 99 && event.movementX > 0) {
+          return 99;
+        } else if (position >= 0 && position <= 99) {
+          return position + event.movementX;
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mouseup", onMouseUp);
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mousemove", onMouseMove);
+    return () => {
+      document.removeEventListener("mouseup", onMouseUp);
+      document.removeEventListener("mousedown", onMouseDown);
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+  }, [onMouseMove, onMouseDown, onMouseUp]);
+
   return (
-    <div className="style-container">
-      <div className="range-base">
-        <div className="range-left"></div>
-        <div className="default-ellipse">
-          <div className="default-text">3.5</div>
+    <div className="container">
+      <div className="track">
+        <div className="thumb" ref={thumbRef} style={{ left: `${position}%` }}>
+          <div className="text">{position}</div>
         </div>
       </div>
     </div>
